1ea255802786ad432b1c27ee8d0d64619ca900ec
kenekizzy <ndukasquad@gmail.com>
2022-02-10 04:53:48 +0100

    Final edit

--- script.js
+++ script.js
@@ -6,52 +6,50 @@ const newQuote = document.querySelector("#new-quote")
 const twitterBtn = document.querySelector("#twitter-btn")
 const loader = document.querySelector("#loader")
 
+//Setting the API quotes array
 let apiQuotes = []
-//loading function
+//function to show loading bar if the quote isn't gotten yet
 function loading(){
     loader.hidden = false
     quoteContainer.hidden = true
 }
+//Function that completes the loading function
 function complete(){
     loader.hidden = true
     quoteContainer.hidden = false
 }
-//getting new quotes
-function newQuotes(array){
-    let random = Math.floor(Math.random() * array.length)
-    //checking the quote length to detemine the styling
-    if(array[random].text.length > 100){
-        quote.classList.add(".long-quote")
-    }else{
-        quote.classList.remove(".long-quote")
-    }
-    quote.textContent = array[random].text
-    author.textContent = array[random].author
-}
-
-//Getting the Quotes from the API
+//Getting the Quotes from the API using Asynchronous functions
 async function getQuotes(){
     loading()
     const apiUrl = "https://type.fit/api/quotes"
     try {
         const response = await fetch(apiUrl)
         apiQuotes = await response.json()
+        let random = Math.floor(Math.random() * apiQuotes.length)
+        //Applying a style class if the quote text is longer than 100
+        if(apiQuotes[random].text.length > 100){
+            quote.classList.add(".long-quote")
+        }else{
+            quote.classList.remove(".long-quote")
+        }
         complete()
-        newQuotes(apiQuotes)
+        //Populating the HTML with the text and the autho
+        quote.textContent = apiQuotes[random].text
+        author.textContent = apiQuotes[random].author   
+        // newQuotes(apiQuotes)
     } catch (error) {
         alert("Error: " + error)
     }
 }
-
-getQuotes()
-
-
 //Tweeting Quotes
 function tweetQuote(){
     const tweetQuote = `https://twitter.com/intent/tweet?text=${quote.textContent} - ${author.textContent}`
     window.open(tweetQuote, "_blank")
 }
 
+//Adding the event listener
 twitterBtn.addEventListener("click", tweetQuote)
-
 newQuote.addEventListener("click", getQuotes)
+
+//On Loading the page
+getQuotes()
